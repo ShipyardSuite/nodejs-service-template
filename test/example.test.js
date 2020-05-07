83bdee3e63fad7357ea3fb175508aaebb523f138
Frederik Reich <frederikreich@googlemail.com>
2020-05-07 11:18:32 +0200

    Add tests

---
+++ test/example.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Calculator', () => {
+    describe('Addition', () => {
+        it('1 should be 1', () => {
+            const num = 1;
+            expect(num).to.equal(1)
+        })
+    })
+})
