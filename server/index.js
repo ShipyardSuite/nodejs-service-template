01418cae3ce0dc352aa080a8d3ca8dbc7e871bf3
Frederik Reich <frederikreich@googlemail.com>
2020-05-12 19:31:05 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const { exampleFunction } = require('./utils/example');
+const express = require("express");
+const { exampleFunction } = require("./utils/example");
 
 const app = express()
 const serviceName = exampleFunction();
 const servicePort = process.env.SERVICE_PORT;
 
-app.get('/', (req, res) => res.send(`Hello from ${serviceName}!`))
+app.get("/", (req, res) => {
+    res.send(`Hello from ${serviceName}!`)
+});
 
-app.listen(servicePort, () => console.log(`Example app listening at http://localhost:${servicePort}`))
+app.get("/health", (req, res) => {
+    res.json({
+        "statusCode": 200,
+        "status": "OK"
+    })
+});
+
+app.listen(servicePort, () => console.log(`Example app listening at http://localhost:${servicePort}`));
