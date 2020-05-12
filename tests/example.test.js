d94232245818a95b9bc1e2d41bdda1cd2a9a43d1
Frederik Reich <frederikreich@googlemail.com>
2020-05-12 19:29:55 +0200

    Update example.test.js

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const { exampleFunction } = require('./../server/utils/example');
+const { exampleFunction } = require("./../server/utils/example");
 
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-describe('Example', function () {
-    describe('exampleFunction()', function () {
-        it('output should equal "testService"', function () {
+describe("Example", function () {
+    describe("exampleFunction()", function () {
+        it("output should equal 'testService'", function () {
             expect(exampleFunction()).to.equal("testService");
         });
     });
